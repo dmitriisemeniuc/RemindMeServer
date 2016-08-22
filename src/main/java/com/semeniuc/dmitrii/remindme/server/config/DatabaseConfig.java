6e77d44e913cadc595e83bd235d24007c0c03087
Dmitrii Semeniuc <dmitriisemeniuc@gmail.com>
2016-08-22 18:10:06 +0300

    [RMS-3] DataSource and EntityManager configuration.

--- src/main/java/com/semeniuc/dmitrii/remindme/server/config/DatabaseConfig.java
+++ src/main/java/com/semeniuc/dmitrii/remindme/server/config/DatabaseConfig.java
@@ -1,13 +1,21 @@
 package com.semeniuc.dmitrii.remindme.server.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Created by Dmitrii on 8/22/2016.
@@ -21,5 +29,37 @@ public class DatabaseConfig {
 
     @Resource
     private Environment env;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalContainerEntityManagerFactoryBean en = new LocalContainerEntityManagerFactoryBean();
+        en.setDataSource(dataSource());
+        en.setPackagesToScan(env.getRequiredProperty("db.entity.package"));
+        en.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        en.setJpaProperties(getHibernateProperties());
+        return null;
+    }
+
+    private Properties getHibernateProperties() {
+        try {
+            Properties properties = new Properties();
+            InputStream is = getClass().getClassLoader().getResourceAsStream("hibernate.properties");
+            properties.load(is);
+
+            return properties;
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Cannot find hibernate.properties file", e);
+        }
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(env.getRequiredProperty("db.url"));
+        ds.setDriverClassName(env.getRequiredProperty("db.driver"));
+        ds.setUsername(env.getRequiredProperty("db.username"));
+        ds.setPassword(env.getRequiredProperty("db.password"));
+        return ds;
+    }
 
 }
